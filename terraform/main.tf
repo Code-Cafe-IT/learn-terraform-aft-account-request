8323c5c00c72de74f02a51973d242c48934a4b57
Đức Lã Minh <laminhduc2k1@gmail.com>
2024-08-24 00:26:41 +0700

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,21 +2,31 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail = "aft.terraform.1@amazon.com"
+    AccountName  = "terraform-aft-1"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "Sandbox"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "aft.terraform.1@amazon.com"
+    SSOUserFirstName = "Sandbox"
+    SSOUserLastName  = "AFT"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "ABC:Owner"       = "aft.terraform.1@amazon.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = "Sandbox AFT"
+    change_reason       = "testing the account vending process 1"
   }
 
   custom_fields = {
