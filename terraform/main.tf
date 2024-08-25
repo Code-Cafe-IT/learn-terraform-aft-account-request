305d0e77cf6f8c74d92383ca0cd5eb9028d7fa71
Đức Lã Minh <laminhduc2k1@gmail.com>
2024-08-25 16:22:40 +0700

    add account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,26 +2,19 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "aft.terraform.2@amazon.com"
-    AccountName  = "terraform-aft-2"
+    AccountEmail = "aft.terraform.10@amazon.com"
+    AccountName  = "Sandbox-aft-10"
     # Syntax for top-level OU
-    ManagedOrganizationalUnit = "Sandbox"
+    ManagedOrganizationalUnit = "Sandbox-AFT"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "aft.terraform.2@amazon.com"
+    SSOUserEmail     = "aft.terraform.10@amazon.com"
     SSOUserFirstName = "Sandbox"
     SSOUserLastName  = "AFT"
   }
 
   account_tags = {
     "ABC:Owner"       = "aft.terraform.2@amazon.com"
-    "ABC:Division"    = "ENT"
-    "ABC:Environment" = "Dev"
-    "ABC:CostCenter"  = "123456"
-    "ABC:Vended"      = "true"
-    "ABC:DivCode"     = "102"
-    "ABC:BUCode"      = "ABC003"
-    "ABC:Project"     = "123456"
   }
 
   change_management_parameters = {
