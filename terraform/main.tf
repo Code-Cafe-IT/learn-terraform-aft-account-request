fa62ecb35216c3a33dcc4c9087eda92f6953a1f1
Đức Lã Minh <laminhduc2k1@gmail.com>
2024-08-25 17:06:31 +0700

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,24 +2,24 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "aft.terraform.9@amazon.com"
-    AccountName  = "Sandbox-aft-9"
+    AccountEmail = "aft.terraform.13@amazon.com"
+    AccountName  = "Sandbox-aft-13"
     # Syntax for top-level OU
     ManagedOrganizationalUnit = "Sandbox"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "aft.terraform.9@amazon.com"
+    SSOUserEmail     = "laminhduc2k1@gmail.com"
     SSOUserFirstName = "Sandbox"
     SSOUserLastName  = "AFT"
   }
 
   account_tags = {
-    "ABC:Owner"       = "aft.terraform.9@amazon.com"
+    "ABC:Owner"       = "aft.terraform.13@amazon.com"
   }
 
   change_management_parameters = {
-    change_requested_by = "Sandbox AFT 9"
-    change_reason       = "testing the account vending process 9"
+    change_requested_by = "Sandbox AFT 13"
+    change_reason       = "testing the account vending process 13"
   }
 
   custom_fields = {
@@ -33,24 +33,24 @@ module "management" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "aft.terraform.11@amazon.com"
+    AccountEmail = "aft.terraform.12@amazon.com"
     AccountName  = "Sandbox-aft-11"
     # Syntax for top-level OU
     ManagedOrganizationalUnit = "Sandbox"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "aft.terraform.11@amazon.com"
+    SSOUserEmail     = "laminhduc2k1@gmail.com"
     SSOUserFirstName = "Sandbox"
     SSOUserLastName  = "AFT"
   }
 
   account_tags = {
-    "ABC:Owner"       = "aft.terraform.11@amazon.com"
+    "ABC:Owner"       = "aft.terraform.12@amazon.com"
   }
 
   change_management_parameters = {
-    change_requested_by = "Sandbox AFT 11"
-    change_reason       = "testing the account vending process 11"
+    change_requested_by = "Sandbox AFT 12"
+    change_reason       = "testing the account vending process 12"
   }
 
   custom_fields = {
