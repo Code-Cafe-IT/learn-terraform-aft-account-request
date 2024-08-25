a668961f1708bae257d0951b1eaadd1d01753c51
Đức Lã Minh <laminhduc2k1@gmail.com>
2024-08-25 15:22:55 +0700

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,19 +2,19 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "aft.terraform.1@amazon.com"
+    AccountEmail = "aft.terraform.2@amazon.com"
     AccountName  = "terraform-aft-1"
     # Syntax for top-level OU
     ManagedOrganizationalUnit = "Sandbox"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "aft.terraform.1@amazon.com"
+    SSOUserEmail     = "aft.terraform.2@amazon.com"
     SSOUserFirstName = "Sandbox"
     SSOUserLastName  = "AFT"
   }
 
   account_tags = {
-    "ABC:Owner"       = "aft.terraform.1@amazon.com"
+    "ABC:Owner"       = "aft.terraform.2@amazon.com"
     "ABC:Division"    = "ENT"
     "ABC:Environment" = "Dev"
     "ABC:CostCenter"  = "123456"
@@ -26,7 +26,7 @@ module "sandbox" {
 
   change_management_parameters = {
     change_requested_by = "Sandbox AFT"
-    change_reason       = "testing the account vending process 1"
+    change_reason       = "testing the account vending process 2"
   }
 
   custom_fields = {
